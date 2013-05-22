8daad4f2efd36bc3bc00392bab650ec1059a84cc
Thomas Meyer <meyertee@gmail.com>
2013-05-22 20:00:05 +0200

    removed footer menu

--- footer.php
+++ footer.php
@@ -2,11 +2,13 @@
 
 				<div id="inner-footer" class="wrap clearfix">
 
+					<!--
 					<nav role="navigation">
-							<?php bones_footer_links(); ?>
+							<?php // bones_footer_links(); ?>
 									</nav>
+					-->
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+						<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
 
 				</div> <!-- end #inner-footer -->
 
