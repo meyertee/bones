48caaaa9cf89445dbe464c452af928033a6b415b
Thomas Meyer <meyertee@gmail.com>
2013-05-22 20:08:28 +0200

    added susy & product/dev settings

--- library/scss/config.rb
+++ library/scss/config.rb
@@ -3,22 +3,30 @@
 # quickly dive right in.
 # For more info about compass + SASS: http://net.tutsplus.com/tutorials/html-css-techniques/using-compass-and-sass-for-css-in-your-next-project/
 
+require "susy"
 
 #########
 # 1. Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "./"
 
-# 2. probably don't need to touch these
+# 2. Settings for the compiler: http://compass-style.org/help/tutorials/configuration-reference/
 css_dir = "../css"
+if (environment == :development)
+  css_path = "/Users/meyertee/Sites/wordpress/wp-content/themes/formpflege/library/css"
+end
+
 sass_dir = "./"
 images_dir = "../images"
 javascripts_dir = "../js"
-environment = :development
 relative_assets = true
+if (environment == :development)
+  relative_assets = false
+end
 
 
 # 3. You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded
+#output_style = :expanded
+output_style = (environment == :production) ? :compressed : :expanded
 
 # 4. When you are ready to launch your WP theme comment out (3) and uncomment the line below
 # output_style = :compressed
