75bbde4ab24446374bfadd47183a27cdf1e2373f
Thomas Meyer <meyertee@gmail.com>
2013-06-24 15:01:22 +0200

    removed copyright in footer

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 									</nav>
 					-->
 
-						<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+					<!-- <p class="source-org copyright">&copy; <?php // echo date('Y'); ?> <?php // bloginfo('name'); ?>.</p> -->
 
 				</div> <!-- end #inner-footer -->
 
