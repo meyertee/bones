aff6f28bf3abc5c83cae85d2ae00a2974bf61dc8
Thomas Meyer <meyertee@gmail.com>
2013-07-27 13:14:11 +0200

    added js for responsive dropdown navi

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -29,46 +29,30 @@ if (!window.getComputedStyle) {
 
 // as the page loads, call these scripts
 jQuery(document).ready(function($) {
-
-    /*
-    Responsive jQuery is a tricky thing.
-    There's a bunch of different ways to handle
-    it, so be sure to research and find the one
-    that works for you best.
-    */
-    
-    /* getting viewport width */
-    var responsive_viewport = $(window).width();
-    
-    /* if is below 481px */
-    if (responsive_viewport < 481) {
-    
-    } /* end smallest screen */
-    
-    /* if is larger than 481px */
-    if (responsive_viewport > 481) {
-        
-    } /* end larger than 481px */
-    
-    /* if is above or equal to 768px */
-    if (responsive_viewport >= 768) {
-    
-        /* load gravatars */
-        $('.comment img[data-gravatar]').each(function(){
-            $(this).attr('src',$(this).attr('data-gravatar'));
-        });
-        
-    }
-    
-    /* off the bat large screen actions */
-    if (responsive_viewport > 1030) {
-        
-    }
-    
+	
+	var $projectMenu = $(".sidebar .widget:eq(1) .menu-projekte-container");
+	var $projectToggle = $(".sidebar .widget:eq(1) .widgettitle");
+	$projectToggle.click(function(ev){
+		if ($projectMenu.css("position") === "absolute"){
+			if ($projectMenu.is(":visible")){
+				$projectMenu.fadeOut("fast");
+				$projectToggle.removeClass("is-open");
+			} else {
+				$projectMenu.show();
+				$projectToggle.addClass("is-open");
+			}
+		}
+		ev.preventDefault();
+		ev.stopPropagation();
+	});
+	$("body").mousedown(function(){
+		if ($projectMenu.css("position") === "absolute" && $projectMenu.is(":visible")){
+			$projectMenu.fadeOut("fast");
+			$projectToggle.removeClass("is-open");
+		}
+	});
 	
 	// add all your scripts here
-	
- 
 }); /* end of as page load scripts */
 
 
